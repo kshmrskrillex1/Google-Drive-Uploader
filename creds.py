38be4514861aef1add699d031135f91597005692
kshmrskrillex1 <88226146+kshmrskrillex1@users.noreply.github.com>
2021-08-22 00:23:03 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 import os
 
 class Creds():
-    TG_TOKEN = os.environ.get("1906448028:AAFXHYWGf6Os33dPeTwfdp7L8hEWvkPgjF8")
+    TG_TOKEN = os.environ.get("1959637102:AAHG-dvYwwAEaDGFN6H7BlC7r8Qv36dI5ig")
